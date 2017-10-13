45a61e314a092df5b890bdab6e48f193879e7fa1
Deepak <deepak3823@gmail.com>
2017-10-13 23:44:42 +0530

    Add function to handle the map load error.

--- js/app.js
+++ js/app.js
@@ -1,41 +1,45 @@
 // array of all the malls in hyderabad
 var malls = [
 {
-	title : 'GVK One',
-	lat : 17.4232521,
-	lng : 78.447205
+    title : 'GVK One',
+    lat : 17.4232521,
+    lng : 78.447205
 },
 {
-	title : 'Inorbit Mall',
-	lat : 17.4356584,
-	lng : 78.3854069
-}
+    title : 'Inorbit Mall',
+    lat : 17.4356584,
+    lng : 78.3854069
+},
 {
-	title : 'Central Shopping Mall',
-	lat : 17.4247671,
-	lng : 78.4073366
-}
+    title : 'Central Shopping Mall',
+    lat : 17.4247671,
+    lng : 78.4073366
+},
 {
-	title : 'Forum Srujana Mall',
-	lat : 17.4247671,
-	lng : 78.4073366
-}
+    title : 'Forum Srujana Mall',
+    lat : 17.4247671,
+    lng : 78.4073366
+},
 {
-	title : 'Manjeera Mall',
-	lat : 17.4247671,
-	lng : 78.4073366
-}
+    title : 'Manjeera Mall',
+    lat : 17.4247671,
+    lng : 78.4073366
+},
 {
-	title : 'Atrium Mall',
-	lat : 17.4247671,
-	lng : 78.4073366
-}
+    title : 'Atrium Mall',
+    lat : 17.4247671,
+    lng : 78.4073366
+},
 ]
 // Initialize the map
 var map;
 function initMap(){
-	map = new google.maps.Map(document.getElementById('map'), {
-		center : {lat : 17.4338554, lng : 78.363027},
-		zoom : 11
-	});
+    map = new google.maps.Map(document.getElementById('map'), {
+        center : {lat : 17.4338554, lng : 78.363027},
+        zoom : 11
+    });
+}
+
+function showError(){
+    document.getElementsByTagName("body").innerHtml = '<h1>Sorry, Google maps is not reachable currently.<br/>Please try Again later</h1>';
 }
