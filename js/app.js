d59f130b0136eb5e340e9a410bcd569cb11680c6
Deepak <deepak3823@gmail.com>
2017-10-13 20:01:20 +0530

    Add latlongs of the malls as an array.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,37 @@
-//Initialize the map
+// array of all the malls in hyderabad
+var malls = [
+{
+	title : 'GVK One',
+	lat : 17.4232521,
+	lng : 78.447205
+},
+{
+	title : 'Inorbit Mall',
+	lat : 17.4356584,
+	lng : 78.3854069
+}
+{
+	title : 'Central Shopping Mall',
+	lat : 17.4247671,
+	lng : 78.4073366
+}
+{
+	title : 'Forum Srujana Mall',
+	lat : 17.4247671,
+	lng : 78.4073366
+}
+{
+	title : 'Manjeera Mall',
+	lat : 17.4247671,
+	lng : 78.4073366
+}
+{
+	title : 'Atrium Mall',
+	lat : 17.4247671,
+	lng : 78.4073366
+}
+]
+// Initialize the map
 var map;
 function initMap(){
 	map = new google.maps.Map(document.getElementById('map'), {
